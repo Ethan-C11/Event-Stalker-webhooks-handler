3c83ea63e64ead59ecfc4a0d4607cd9e69f59b21
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-01-22 13:38:52 +0100

    feat: hook interceptor

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const PORT = 3000
+app.use(bodyParser.json())
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+app.get("/", (req, res) => {
+    res.send("Serveur en ligne !");
+});
+
+app.post("/hook", (req, res) => {
+    console.log(req.body)
+    res.status(200).json("Message reçu").end()
+})
